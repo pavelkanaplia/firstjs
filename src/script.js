4cd404ce02aedafc7222b7b5354668ba017ca22e
Pavel <boyskouterockefeller@gmail.com>
2021-10-05 19:33:01 +0300

    break and continue

--- src/script.js
+++ src/script.js
@@ -1,76 +1,9 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
 'use strict';
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms, 
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+let num = 50;
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('NoNoNo');
+while (num < 55 ) {
+    console.log(num);
+    num++;
 }
 
-
-// if (num < 49 ) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log ('To much');
-// } else {
-//     console.log('Ok!');
-// }
-
-
-// (num === 50) ? console.log('Ok!') : console.log('Error'); 
-
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('Nonono');
-        break;
-    case 100:
-        console.log('Nonono');
-        break;
-    case 51:
-        console.log('Yes, right!');
-        break;
-    default:
-        console.log('Do not this time');
-        break;
-}
