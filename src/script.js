b336b7897fe4c09fe5398854caa4a642fde3c15b
Pavel <boyskouterockefeller@gmail.com>
2021-10-06 21:49:34 +0300

    Method

--- src/script.js
+++ src/script.js
@@ -1,34 +1,7 @@
 'use strict';
-let num = 20;
 
-function showFirstMessage(text) {
-    console.log(text);
-    console.log(num);
-}
+const str = "testing";
 
-showFirstMessage('Hello world');
-console.log(num);
 
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log("Hello");
-};
-
-logger();
-
-const calc = (a, b) => a + b;
+console.log(str.toUpperCase());
+console.log(str);
