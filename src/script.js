2401ecd613ba67cef765bc0c34977d1c62c355ec
Pavel <boyskouterockefeller@gmail.com>
2021-10-06 21:50:45 +0300

    Method

--- src/script.js
+++ src/script.js
@@ -1,7 +1,29 @@
 'use strict';
 
-const str = "testing";
+const str = "tesTIng";
 
 
-console.log(str.toUpperCase());
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
 console.log(str);
+
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("t"));
+
+const logg = "Hello world!";
+
+console.log(logg.slice(0, 5));
+
+console.log(logg.substring(0, 5));
+
+console.log(logg.substr(0, 5));
+
+const num = 12.5;
+console.log(Math.round(num));
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
