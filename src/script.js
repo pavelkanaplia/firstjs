6674119e7da4ebcd35db4084170458e584119fd4
Pavel <boyskouterockefeller@gmail.com>
2021-10-13 19:20:57 +0300

    task

--- src/script.js
+++ src/script.js
@@ -1,47 +1,23 @@
 'use strict';
 
+// let x = 5; alert( x++ );
+// [ ] + false - null + true
+// console.log([] + false - null + true)); // "NaN"
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+// console.log([] + 1 + 2); //"12"
+// alert( "1"[0] );
+// 
+// И(&&) запинается на лжи
+// ИЛИ(||) запинается на правде
+// console.log(!!( 1 && 2 ) === (1 && 2 ));
+// alert( null || 2 && 3 || 4 ); //ответ 3
 
-// To string
-// // 1)
-// console.log(typeof(String(null)));
-//2)
-console.log(typeof(5 + ''));
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
 
-const num = 5;
-console.log("https:/vk.com/catalog/" + num);
-
-const fontSize = 26 + 'px';
-console.log(fontSize);
-
-// To number
-// 1)
-console.log(typeof(Number('4')));
-
-//2)
-console.log(typeof(+'6'));
-
-//3)
-console.log(typeof(parseInt("15px", 10)));
-
-let answ = +prompt("Hello", "");
-
-// To boolen
-
-// 0, '', null, undefined, NaN; //false
-//1)
-let switcher = null;
-if (switcher) {
-    console.log('Working.....');
-}
-
-switcher = 1;
-if (switcher) {
-    console.log('Working.....');
-}
-
-//2)
-console.log(typeof(Boolean('4')));
-
-//3)
-
-console.log(typeof(!!"44444"));
+// console.log(a == b);
+// alert( +"Infinity");
+// 
+console.log(0 || "" || 2 || undefined || true || false);
